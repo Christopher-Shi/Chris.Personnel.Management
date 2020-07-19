6657ef7fbb8f1bb38cf674fc12ac2dcf705fe17d
Christopher-Shi <62409809+Christopher-Shi@users.noreply.github.com>
2020-07-19 13:12:28 +0800

    added xunit  code for my projecct

---
+++ Back-end-code/Chris.Personnel.Management.Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace Chris.Personnel.Management.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ShouldAdd()
+        {
+            //Arrange
+            var sut = new Calculator();//sut-System Under Test
+
+            //Act
+            var result = sut.Add(1, 2);
+
+            //Assert
+            Assert.Equal(3, result);
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
