8bb7350853b556ab16434fdab8d7c6cf2ca95f32
Christopher-Shi <62409809+Christopher-Shi@users.noreply.github.com>
2020-08-12 23:38:34 +0800

    edit code

--- Back-end-code/Chris.Personnel.Management.Blazor/Pages/User.razor.cs
+++ Back-end-code/Chris.Personnel.Management.Blazor/Pages/User.razor.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using AntDesign;
 using Chris.Personnel.Management.Blazor.Services;
-using Chris.Personnel.Management.UICommand;
-using Chris.Personnel.Management.UICommand.DTO;
 using Chris.Personnel.Management.ViewModel;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Forms;
 
 namespace Chris.Personnel.Management.Blazor.Pages
 {
@@ -18,53 +11,7 @@ namespace Chris.Personnel.Management.Blazor.Pages
         [Inject]
         public IUserService UserService { get; set; }
 
-        [Inject]
-        public ModalService ModalService { get; set; }
-
         public IEnumerable<UserFormViewModel> Users { get; set; } = new List<UserFormViewModel>();
-
-        //public UserPaginationViewModel UsersByPage { get; set; } = new UserPaginationViewModel();
-
-        public UserAddUICommand UserAddUICommand { get; set; } = new UserAddUICommand
-        {
-            User = new UserDTO()
-        };
-
-        public string txtValue { get; set; }
-
-        private bool _editModalVisible;
-        private bool _addModalVisible;
-
-        private void ShowEditModal()
-        {
-            _editModalVisible = true;
-        }
-
-        private void HideEditModal()
-        {
-            _editModalVisible = false;
-        }
-
-        private void ShowAddModal()
-        {
-            _addModalVisible = true;
-        }
-
-        private async Task HideAddModal()
-        {
-            await UserService.AddUser(UserAddUICommand);
-            _addModalVisible = false;
-        }
-
-        private async Task OnFinish()
-        {
-            await UserService.AddUser(UserAddUICommand);
-        }
-
-        private void OnFinishFailed(EditContext editContext)
-        {
-            Console.WriteLine($"Failed:{JsonSerializer.Serialize(UserAddUICommand)}");
-        }
 
         protected override async Task OnInitializedAsync()
         {
--- Back-end-code/Chris.Personnel.Management.Blazor/Program.cs
+++ Back-end-code/Chris.Personnel.Management.Blazor/Program.cs
@@ -1,26 +1,13 @@
 using System;
-using System.Configuration;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Chris.Personnel.Management.Blazor.Services;
-using Chris.Personnel.Management.Common.Helper;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
 
 namespace Chris.Personnel.Management.Blazor
 {
     public class Program
     {
-        //public Startup(IConfiguration configuration)
-        //{
-        //    Configuration = configuration;
-        //}
-
-        [Inject]
-        public static IConfiguration Configuration { get; }
-
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -39,8 +26,6 @@ namespace Chris.Personnel.Management.Blazor
 
             builder.Services.AddHttpClient<IUserService, UserService>(
                 client => client.BaseAddress = new Uri("https://localhost:5001"));
-
-            builder.Services.AddAntDesign();
 
             await builder.Build().RunAsync();
         }
