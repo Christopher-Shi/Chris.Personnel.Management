1637ba53c9a95b2a4493d9d19e02268bcbc4144c
shixiaoyong <sxy0129@yeah.net>
2020-08-14 13:50:37 +0800

    bootstrapblazor

--- Back-end-code/Chris.Personnel.Management.Blazor/Pages/User.razor.cs
+++ Back-end-code/Chris.Personnel.Management.Blazor/Pages/User.razor.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using BootstrapBlazor.Components;
 using Chris.Personnel.Management.Blazor.Services;
 using Chris.Personnel.Management.ViewModel;
 using Microsoft.AspNetCore.Components;
@@ -12,6 +13,8 @@ namespace Chris.Personnel.Management.Blazor.Pages
         public IUserService UserService { get; set; }
 
         public IEnumerable<UserFormViewModel> Users { get; set; } = new List<UserFormViewModel>();
+
+        public UserFormViewModel SearchModel { get; set; } = new UserFormViewModel();
 
         protected override async Task OnInitializedAsync()
         {
