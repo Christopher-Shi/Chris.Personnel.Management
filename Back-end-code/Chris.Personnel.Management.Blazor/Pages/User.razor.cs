4e4fcd343d950d97ffed6c6a8277bf84b302a0f8
Christopher-Shi <62409809+Christopher-Shi@users.noreply.github.com>
2020-08-15 23:21:31 +0800

    commit code

--- Back-end-code/Chris.Personnel.Management.Blazor/Pages/User.razor.cs
+++ Back-end-code/Chris.Personnel.Management.Blazor/Pages/User.razor.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using BootstrapBlazor.Components;
 using Chris.Personnel.Management.Blazor.Services;
+using Chris.Personnel.Management.UICommand;
+using Chris.Personnel.Management.UICommand.DTO;
 using Chris.Personnel.Management.ViewModel;
 using Microsoft.AspNetCore.Components;
 
@@ -15,6 +17,14 @@ namespace Chris.Personnel.Management.Blazor.Pages
         public IEnumerable<UserFormViewModel> Users { get; set; } = new List<UserFormViewModel>();
 
         public UserFormViewModel SearchModel { get; set; } = new UserFormViewModel();
+
+        public Modal AddModal { get; set; }
+        public Modal EditModal { get; set; }
+
+        public UserAddUICommand UserAdd { get; set; } = new UserAddUICommand
+        {
+            User = new UserDTO()
+        };
 
         protected override async Task OnInitializedAsync()
         {
